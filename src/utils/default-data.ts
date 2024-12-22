481421c425688847214454f4ed2341b7e47d37f8
talfedev <talfedev@gmail.com>
2024-12-22 17:40:32 +0700

    fix: remove default values

--- src/utils/default-data.ts
+++ src/utils/default-data.ts
@@ -1,25 +1,14 @@
 import type { Stock, CuttingOptions } from "../types/types"
 
 export const stock: Stock = {
-    sizes: [1000,2000],
-    quantities: {
-        1000: 1,
-        2000: 2
-    }
+    sizes: [],
+    quantities: {}
 }
 
 export const defaultCutOptions: CuttingOptions[] = [{
-    totalMaterialLeft: '0',
+    totalMaterialLeft: 0,
     remainingStock: [],
     cutMap: {}
 }]
 
-export const pieces: number[] = [
-    100,
-    200,
-    300,
-    750,
-    1050,
-    // 1900,
-    // 500
-]
+export const pieces: number[] = [];
